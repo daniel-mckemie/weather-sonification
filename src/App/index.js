0ab9ea6d6f714d3a49d2bbce0fab56c796337fd8
Daniel McKemie <daniel.mckemie@gmail.com>
2018-11-27 18:17:16 -0500

    initial commit

---
+++ src/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+import LocForm from '../LocForm/'
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+    <LocForm />
+    </div>
+    )
+    
+    
+
+  }
+}
+
+export default App;
