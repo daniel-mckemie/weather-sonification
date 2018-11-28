8c5b093a8801b19208dd44cd27a88fa72446289b
Daniel McKemie <daniel.mckemie@gmail.com>
2018-11-28 02:10:14 -0500

    sound and data set for change

--- src/App/index.js
+++ src/App/index.js
@@ -19,32 +19,38 @@ import LocForm from "../LocForm/";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends Component {
-	// constructor() {
-	// 	super();
-	// 	this.nodeCache = [
-	// 		<ROscillator
-	// 			start={1}
-	// 			key={1}
-	// 			frequency={440}
-	// 			type="triangle"
-	// 			detune={0}
-	// 		/>,
-	// 		<RBiquadFilter
-	// 			key={2}
-	// 			frequency={600}
-	// 			type="lowpass"
-	// 			detune={0}
-	// 			transitionDuration={0.8}
-	// 		/>,
-	// 		<RStereoPanner key={3} />
-	// 	];
+	constructor() {
+		super();
+		this.nodeCache = [
+			<ROscillator
+				start={1}
+				key={1}
+				frequency={440}
+				type="triangle"
+				detune={0}
+			/>,
+			<RBiquadFilter
+				key={2}
+				frequency={600}
+				type="lowpass"
+				detune={0}
+				transitionDuration={0.8}
+			/>,
+			<RStereoPanner key={3} />
+		];
 
-	// 	// this.state = {
-	// 	// 	nodes: this.nodeCache,
-	// 	// 	toggle: true,
-	// 	// 	freq: 440
-	// 	// };
-	// }
+		this.state = {
+			nodes: this.nodeCache,
+			toggle: true,
+			freq: 440
+		};
+
+		this.change = () => {
+      const changed = this.nodeCache.slice();
+      changed.splice(1, 1, <RGain key={2} gain={0.5} />);
+      this.setState({ nodes: changed });
+    };
+	}
 
 	state = {
 		temperature: undefined,
@@ -52,7 +58,7 @@ class App extends Component {
 		country: undefined,
 		humidity: undefined,
 		description: undefined,
-		error: undefined
+		error: undefined,
 	};
 
 	//getWeather is a method we'll use to make the api call
@@ -83,22 +89,39 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<LocForm loadWeather={this.getWeather} />
-				<Weather
-					temperature={this.state.temperature}
-					city={this.state.city}
-					country={this.state.country}
-					humidity={this.state.humidity}
-					description={this.state.description}
-					error={this.state.error}
-				/>
-
+				<div className="wrapper">
+					<div className="main">
+						<div className="container">
+							<div className="row">
+								<div className="col-xs-5 title-container" />
+								<div className="col-xs-7 form-container">
+									<LocForm loadWeather={this.getWeather} />
+									<Weather
+										temperature={this.state.temperature}
+										city={this.state.city}
+										country={this.state.country}
+										humidity={this.state.humidity}
+										description={this.state.description}
+										error={this.state.error}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<RAudioContext debug={true}>
-					<RPipeline>
-						<ROscillator start={0} frequency={440} type="sine" detune={0} />
-						<RGain gain={0.1} />
-					</RPipeline>
-				</RAudioContext>
+        <article>
+          <h1>Mutation</h1>
+          This example demonstrates how <em>r-audio</em> graphs can be mutated via React state.
+          <em>r-audio</em> takes care of reconfiguring the connections and instantiating new nodes as necessary.
+        </article>
+        <RPipeline>
+          <button onClick={this.change}>Mutate audio graph</button>
+          <ROscillator start={0} frequency={440} type="triangle" detune={0} />
+          {this.state.nodes}
+          <RGain gain={0.5} transitionDuration={1} />
+        </RPipeline>
+      </RAudioContext>
 			</div>
 		);
 	}
