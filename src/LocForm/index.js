0f07c0a4dfc8c9c1a3a5ab2fbe875b36c583260e
Daniel McKemie <daniel.mckemie@gmail.com>
2018-11-28 00:31:04 -0500

    weather key fetching properly and hidden

--- src/LocForm/index.js
+++ src/LocForm/index.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 
 import axios from 'axios';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
 class LocForm extends Component {
 	state = {
-		persons: []
+		weather: []
 	}
-	componentDidMount() {
-		axios.get(`https://jsonplaceholder.typicode.com/users`)
+	componentDidMount() {		
+		axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=94612,us&APPID=${API_KEY}`)
 		.then(res => {
-			const persons = res.data;
-			this.setState({ persons });
-			console.log(persons)
+			const weather = res.data;
+			this.setState({ weather });
+			console.log(weather)
+		}).catch(error => {
+			console.log('Error from fetching data', error);
 		})
 	}
 
 	render() {
 		return (
-			<ul>
-				{ this.state.persons.map(person => <li>{person.name}</li>)}
-			</ul>
+			<div>Weather!</div>
 		);
 	}
 }
